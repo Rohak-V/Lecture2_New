d1363e736bb0b354981eb8065ad43d4f2025c667
DESKTOP-EC52BN1\rohak <rohak.v@gmail.com>
2020-05-01 16:19:33 +0100

    Changed

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, escape, request, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    headline = "Hello World!"
-    return render_template("index.html", headline=headline)
-
-
-@app.route("/bye")
-def bye():
-    headline = "Goodbye!"
-    return render_template("index.html", headline=headline)
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template("index.html", new_year=new_year)
